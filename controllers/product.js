c9f850df9e96af7d85440f0e81e498c36a77ac71
ASYDLR.01 <asydlr.01@gmail.com>
2024-10-08 22:10:50 +0800

    Updated Controllers

--- controllers/product.js
+++ controllers/product.js
@@ -2,33 +2,66 @@
 const Product = require("../models/Product");
 const bcrypt = require("bcryptjs");
 const auth = require("../auth");
-const {errorHandler} = require("../auth");
+const { errorHandler } = require("../auth");
 
 // MIDDLEWARES
-module.exports.productFunction01 = (req, res) => {
-
+// CREATE A PRODUCT (/)
+module.exports.createProduct = (req, res) => {
+  let newProduct = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+  });
 };
 
-module.exports.productFunction02 = (req, res) => {
-
+// RETRIEVE ALL PRODUCTS (/ALL)
+module.exports.getAllProducts = (req, res) => {
+  return Course.find({})
+    .then((result) => {
+      if (result.length > 0) {
+        return res.status(200).send({ result });
+      } else {
+        return res.status(404).send({ message: "No Products Found" });
+      }
+    })
+    .catch((error) => errorHandler(error, req, res));
 };
 
-module.exports.productFunction03 = (req, res) => {
-
+//RETRIEVE ALL ACTIVE PRODUCTS (/ACTIVE)
+module.exports.getAllActiveProducts = (req, res) => {
+  Product.find({ isActive: true })
+    .then((result) => {
+      if (result.length > 0) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(404).send(false);
+      }
+    })
+    .catch((error) => errorHandler(error, req, res));
 };
 
-module.exports.productFunction04 = (req, res) => {
-
+//RETRIEVE A SINGLE PRODUCT (/:PRODUCT-ID)
+module.exports.getProduct = (req, res) => {
+  Product.findById(req.params.productId)
+    .then((product) => {
+      if (product) {
+        return res.status(200).send(product);
+      } else {
+        return res.status(404).send(false);
+      }
+    })
+    .catch((error) => errorHandler(error, req, res));
 };
 
-module.exports.productFunction05 = (req, res) => {
 
-};
 
-module.exports.productFunction06 = (req, res) => {
 
-};
+module.exports.productFunction03 = (req, res) => {};
 
-module.exports.productFunction07 = (req, res) => {
+module.exports.productFunction04 = (req, res) => {};
 
-};
+module.exports.productFunction05 = (req, res) => {};
+
+module.exports.productFunction06 = (req, res) => {};
+
+module.exports.productFunction07 = (req, res) => {};
