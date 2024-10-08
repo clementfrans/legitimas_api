992808c4a9e4cec3ce300348629ebfaeb5e45327
delossantos.clementfrancis <delossantos.clementfrancis@gmail.com>
2024-10-08 21:43:37 +0800

    Update Products Controller

---
+++ controllers/product.js
@@ -0,0 +1,34 @@
+// DEPENDENCIES
+const Product = require("../models/Product");
+const bcrypt = require("bcryptjs");
+const auth = require("../auth");
+const {errorHandler} = require("../auth");
+
+// MIDDLEWARES
+module.exports.productFunction01 = (req, res) => {
+
+};
+
+module.exports.productFunction02 = (req, res) => {
+
+};
+
+module.exports.productFunction03 = (req, res) => {
+
+};
+
+module.exports.productFunction04 = (req, res) => {
+
+};
+
+module.exports.productFunction05 = (req, res) => {
+
+};
+
+module.exports.productFunction06 = (req, res) => {
+
+};
+
+module.exports.productFunction07 = (req, res) => {
+
+};
