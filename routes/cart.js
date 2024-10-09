4514b774cd867fda9793fd3ab2c71891a52477a7
delossantos.clementfrancis <delossantos.clementfrancis@gmail.com>
2024-10-09 13:15:26 +0800

    updated cart files

--- routes/cart.js
+++ routes/cart.js
@@ -2,23 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cart");
-
-// ADD ITEMS TO THE CART
-router.post("/add-to-cart", cartController.addToCart);
-
-// UPDATE PRODUCT QUANTITY IN THE CART
-router.put("/update-product-quantity", cartController.updateProductQuantity);
-
-// RETRIEVE THE CART
-router.get("/", cartController.getCart);
+const { verify, verifyAdmin } = require("../auth");
 
 // RETRIEVE USER'S CART
-router.get("/get-cart", cartController.getCart);
+router.get("/get-cart", verify, cartController.getCart);
 
 // ADD ITEMS TO CART
-router.post("/add-to-cart", cartController.addToCart);
+router.post("/add-to-cart", verify, cartController.addToCart);
 
 // UPDATE PRODUCT QUANTITY IN THE CART
-router.patch("/update-cart-quantity", cartController.updateProductQuantity);
+router.patch("/update-cart-quantity", verify, cartController.updateProductQuantity);
 
 module.exports = router;
