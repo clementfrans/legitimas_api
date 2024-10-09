fd138b0e6c3d6b21dd3e91336b3d327cdf32cca4
ASYDLR.01 <asydlr.01@gmail.com>
2024-10-09 12:14:27 +0800

    Merged

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
 const productController = require("../controllers/product");
-const productController = require("../controllers/product");
 const { verify, verifyAdmin } = require("../auth");
 const router = express.Router();
 
@@ -10,10 +9,23 @@ router.post("/", verify, verifyAdmin, productController.createProduct);
 router.get("/all", verify, verifyAdmin, productController.getAllProducts);
 router.get("/active", productController.getAllActiveProducts);
 router.get("/:productId", productController.getProduct);
-router.patch("/:productId/update", verify, verifyAdmin, productController.updateProductInfo);
-router.patch("/:productId/archive", verify, verifyAdmin, productController.archiveProduct);
-router.patch("/:productId/activate", verify, verifyAdmin, productController.activateProduct);
-
-
+router.patch(
+  "/:productId/update",
+  verify,
+  verifyAdmin,
+  productController.updateProductInfo
+);
+router.patch(
+  "/:productId/archive",
+  verify,
+  verifyAdmin,
+  productController.archiveProduct
+);
+router.patch(
+  "/:productId/activate",
+  verify,
+  verifyAdmin,
+  productController.activateProduct
+);
 
 module.exports = router;
